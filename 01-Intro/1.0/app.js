e15ca3621aaae670e49f82bc1bff088dc6cdfdbe
Justine-Frigo <justine0912@gmail.com>
2024-03-29 09:55:57 +0100

    first exercise

---
+++ 01-Intro/1.0/app.js
@@ -0,0 +1,13 @@
+
+let num1 = 20;
+let num2 = 2;
+
+console.log(num1 + num2);
+
+console.log(num1 - num2);
+
+console.log(num1 * num2);
+
+console.log(num1 / num2);
+
+console.log(num1 % num2);
---
+++ 01-Intro/1.1/app.js
@@ -0,0 +1,3 @@
+let text = "Hello BeCode!"
+
+console.log(text);
